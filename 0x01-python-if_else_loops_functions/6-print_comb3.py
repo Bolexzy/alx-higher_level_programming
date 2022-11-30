6252ae1b419c418401195801581c66f612eccb71
Bolexzy <boluoyewumi79@gmail.com>
2022-11-30 02:11:53 +0100

    Add files

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.8
 for num in range(0, 100):
-    if (num % 10) < (num / 10):
+    if (num / 10) > (num % 10):
         continue
     if num == 89:
         print("{:02d}".format(num))
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,6 @@
+def islower(c):
+    for a in range(97, 123):
+        if (ord(c) == a):
+            return True
+    else:
+        return False
