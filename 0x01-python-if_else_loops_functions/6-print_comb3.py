6aa0be455a33d058105055d1635f238bc0804811
Bolexzy <boluoyewumi79@gmail.com>
2022-11-30 02:03:30 +0100

    Add task 6 script

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3.8
+for num in range(0, 100):
+    if (num % 10) < (num / 10):
+        continue
+    if num == 89:
+        print("{:02d}".format(num))
+    else:
+        print("{:02d}".format(num), end=', ')
