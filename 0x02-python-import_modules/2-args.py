867c0bf256b20508e78ecfe89cb20ece07517d12
Bolexzy <boluoyewumi79@gmail.com>
2022-12-01 18:13:48 +0100

    feat " Add 2-args.py "prints arguments using argv"

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    ac = len(argv) - 1
+    if (ac == 1):
+        print("{} argument:".format(ac))
+    else:
+        print("{} arguments:".format(ac))
+
+    for i in range(1, ac + 1):
+        print("{}: {}".format(i, argv[i]))
