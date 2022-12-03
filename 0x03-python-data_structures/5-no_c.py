871699e113f6bb6d225515fd8e45452a77c01d8c
Bolexzy <boluoyewumi79@gmail.com>
2022-12-03 19:55:52 +0100

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     # copy charcters from a string except c and C
-    new_string = [x for x in my_string if not(x in 'cC')]
-    return ("".join(new_string))
+    new = [x for x in my_string if not(x in 'cC')]
+
+    return (''.join(str(e) for e in new))
