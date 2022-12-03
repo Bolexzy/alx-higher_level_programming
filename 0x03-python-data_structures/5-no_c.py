ad5ac3f8cdc115e0c9531112fdff9a976c59077c
Bolexzy <boluoyewumi79@gmail.com>
2022-12-03 20:05:05 +0100

    Fix error 3 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 def no_c(my_string):
     ''' copy charcters from a string except c and C '''
-    new = [x for x in my_string if x != 'c' and x != 'C']
+    my_dict = {ord(i): None for i in 'cC'}
+    new_string = my_string.translate(my_dict)
 
-    return (''.join(str(e) for e in new))
+    return new_string
