c0377aa59fac20545678e84ae6bc817710aca345
Bolexzy <boluoyewumi79@gmail.com>
2022-12-04 16:57:52 +0100

    Add 9-max_integer.py: "inds the biggest integer of a list"

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
+
+    max = my_list[0]
+    for num in my_list:
+        if num > max:
+            max = num
+    return max
