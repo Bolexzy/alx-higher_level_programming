5132ffae56c896499eadfd5a6f65364b694140f0
Bolexzy <boluoyewumi79@gmail.com>
2022-12-04 17:11:11 +0100

    Add 11-delete_at.py deletes the item at a specific position in a list.

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    list_len = len(my_list)
+    if idx < 0 or idx >= list_len:
+        return my_list
+    del my_list[idx]
+    return my_list
