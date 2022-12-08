685ad1155071713c463ed15d706280dee287979e
Bolexzy <boluoyewumi79@gmail.com>
2022-12-08 02:56:14 +0100

    Add 12-roman_to_int.py converts a Roman numeral to an intege

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def roman_to_int(roman_string):
     """Converts a roman numeral to an integer."""
     if (not isinstance(roman_string, str) or
@@ -23,7 +22,7 @@ def roman_to_int(roman_string):
 
         if (i != (len(roman_string) - 1) and
                 roman_dict[roman_string[i]] < roman_dict[roman_string[i + 1]]):
-                num += roman_dict[roman_string[i]] * -1
+            num += roman_dict[roman_string[i]] * -1
 
         else:
             num += roman_dict[roman_string[i]]
