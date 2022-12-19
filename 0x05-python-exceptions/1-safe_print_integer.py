d30d1816aacb77aef313187a82500912a319a015
Bolexzy <boluoyewumi79@gmail.com>
2022-12-19 23:52:48 +0100

    Add 2-safe_print_list_integers.py prints the first x elements of a list and only integers.
    gp

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -10,5 +10,5 @@ def safe_print_integer(value):
     try:
         print("{:d}".format(value))
         return True
-    except ValueError:
+    except (ValueError, TypeError):
         return False
