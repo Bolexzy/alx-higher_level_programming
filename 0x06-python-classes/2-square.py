c41fec267f9cb5405cb044e768248ba846031680
Bolexzy <boluoyewumi79@gmail.com>
2022-12-22 01:17:03 +0100

    Update file: fix error

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-''' A Module that creates a Square object '''
-lass Square:
-    """ A class that defines a square by its size
-    """
+# 2-square.py
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Method to initialize the square object
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = int(size)
+        self.__size = size
