e97ded291d85d74c249846b0c507fd35143f1804
Bolexzy <boluoyewumi79@gmail.com>
2023-01-04 23:38:24 +0100

    Update file: add new method with getattr

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string(l=[]):
-    l += ["BestSchool"]
-    return ", ".join(l)
+    magic_string.count = getattr(magic_string, "count", 0) + 1
+    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
