c58555a29a35349cc81b583a5918128f1e108f91
Bolexzy <boluoyewumi79@gmail.com>
2023-01-11 18:34:28 +0100

    Update file: fix error

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -12,8 +12,8 @@ class Square(Rectangle):
             size (int): The size of the new square.
         """
         self.integer_validator("size", size)
-        super().__init__(self.__size, self.__size)
         self.__size = size
+        super().__init__(self.__size, self.__size)
 
     def area(self):
         """Method that returns the area of the sqaure"""
