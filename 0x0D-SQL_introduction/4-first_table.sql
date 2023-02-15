ce4f300c4354e19b50ab6bf4149a0586cbaba8fd
Bolexzy <boluoyewumi79@gmail.com>
2023-02-15 02:22:04 +0100

    Fix

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- creates a table called first_table in the current database.
-CREATE TABLE first_table IF NOT EXISTS first_table (
+CREATE TABLE IF NOT EXISTS first_table (
     id INT,
     name VARCHAR(256)
     );
