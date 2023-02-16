3fece1aea8d138d48f3fe062d43ce34c3d203d49
Bolexzy <boluoyewumi79@gmail.com>
2023-02-16 19:42:11 +0100

    fix

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,9 +1,9 @@
 -- Creates the MySQL server user user_0d_1.
 -- GRant user_od_1 all privileges.
 CREATE USER
-IF NOT EXISTS 'user_0d_1'@'localhost'
+    IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-ON *.*
-TO 'user_0d_1'@'localhost'
-IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL
+    ON *.*
+    TO 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
