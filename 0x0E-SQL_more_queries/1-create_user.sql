8c4ef7d8ac2e409f9ab736d15514b20e889766ec
Bolexzy <boluoyewumi79@gmail.com>
2023-02-16 19:43:27 +0100

    fix-4

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,7 +3,7 @@
 CREATE USER
     IF NOT EXISTS 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL
+GRANT ALL PRIVILEGES
     ON *.*
     TO 'user_0d_1'@'localhost'
     IDENTIFIED BY 'user_0d_1_pwd';
