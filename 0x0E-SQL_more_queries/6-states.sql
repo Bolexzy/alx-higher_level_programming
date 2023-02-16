a4bf37756c65a47b280ac579a3606f3f46d13295
Bolexzy <boluoyewumi79@gmail.com>
2023-02-16 20:07:09 +0100

    Add file
     a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
    
    states description:
    id INT unique, auto generated, can’t be null and is a primary key
    name VARCHAR(256) can’t be null

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Creates the database hbtn_0d_usa and the table states.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+    id  INT AUTO_INCREMENT NOT NULL,
+    PRIMARY KEY(id),
+    name    VARCHAR(256)    NOT NULL
+    );
