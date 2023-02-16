2f61029a37d0183d3e17dea9f365ad2f49bc2152
Bolexzy <boluoyewumi79@gmail.com>
2023-02-16 20:31:13 +0100

    Add file  a script that lists all cities contained in the database hbtn_0d_usa.
    Each record should display: cities.id - cities.name - states.name
    Results must be sorted in ascending order by cities.id

---
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+-- Lists all cities contained in the database hbtn_0d_usa.
+-- Records are sorted in order of ascending cities.id.
+SELECT cities.id, cities.name, states.name
+    FROM cities
+    INNER JOIN states
+    ON cities.state_id = state.id
+    ORDER BY cities.id;
