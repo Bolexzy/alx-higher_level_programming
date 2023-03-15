5fe8a8f6a01e7e849b2f3e3d7de060e78d91e98e
Bolexzy <boluoyewumi79@gmail.com>
2023-03-15 15:23:37 +0100

    Update: fix 2

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-console.log(list.map(x => x * list.indexOf(x)));
+console.log(list.map((item, index) => item * index));
