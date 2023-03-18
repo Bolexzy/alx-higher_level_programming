6d418bcab57a835df6c3cb25e59fc3016ba3280a
Bolexzy <boluoyewumi79@gmail.com>
2023-03-18 13:14:57 +0100

    Update: fix id attributes

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -2,7 +2,7 @@
 # Defines a State model.
 # Inherits from SQLAlchemy Base and links to the MySQL table states.
 
-from sqlalchemy import Column, Integer, String, MetaData
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -16,5 +16,5 @@ class State(Base):
     """
     __tablename__ = 'states'
 
-    id = Column(Integer, unique=True, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
