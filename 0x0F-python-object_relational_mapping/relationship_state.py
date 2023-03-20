21c2ea323994b32f2a3c93e4b50dadd1f7808c4a
Bolexzy <boluoyewumi79@gmail.com>
2023-03-20 15:40:42 +0100

    Update file: update relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -6,8 +6,7 @@ Inherits from SQLAlchemy Base and links to the MySQL table states.
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from relationship_city import Base, City
 
 
 class State(Base):
@@ -22,4 +21,4 @@ class State(Base):
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    city = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship("City", backref="state", cascade="all, delete")
