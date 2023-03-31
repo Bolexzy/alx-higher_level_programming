93962a160cbf2dfaebe2aa9f3f3a18b66ff3d1a0
Bolexzy <boluoyewumi79@gmail.com>
2023-03-31 16:47:30 +0100

    [Update file]:  a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
    A header variable X-School-User-Id must be sent with the value 98

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to the URL using curl, and display the body of the response
+# Sends a GET request to the URL using curl, and displays the body of the response # A header variable X-School-User-Id must be sent with the value 98
 curl -sH "X-School-User-Id: 98" "$1"
