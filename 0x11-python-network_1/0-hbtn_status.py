08b0b621a53a78a90edcb1beb7e72d4ec6923331
Bolexzy <boluoyewumi79@gmail.com>
2023-03-31 22:02:42 +0100

    [Update file]: add docs

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-#
-
+"""Fetches https://intranet.hbtn.io/status."""
 import urllib.request
+
 
 if __name__ == "__main__":
     with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
