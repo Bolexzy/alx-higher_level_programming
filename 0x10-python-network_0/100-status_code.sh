84820f59615cf3a7ccc2936de44f8b11e8c49ba0
Bolexzy <boluoyewumi79@gmail.com>
2023-03-31 18:08:54 +0100

    Add file:  a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
    You are not allowed to use any pipe, redirection, etc.
    You are not allowed to use ; and &&

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a request to a URL, and displays only the status code of the response.
-curl -s -o /dev/null -w "%{http_code}" "$1"
+# Sends a GET request to a URL, and displays only the status code of the response.
+curl -so /dev/null -w "%{http_code}" "$1"
