3f467db300ef2c1e95b632e5c226b30762f25d03
Bolexzy <boluoyewumi79@gmail.com>
2023-05-03 20:11:37 +0100

    Add action: ENTER when the focus is on INPUT#language_code

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+    function getTranslation() {
+       const langCode = $('INPUT#language_code').val();
+       const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+       $.get(url, function(data) {
+           $('DIV#hello').text(data.hello);
+       });
+    }
+   $('INPUT#btn_translate').click(getTranslation());
+   $('INPUT#language_code').keypress(function(event) {
+       if (event.keyCode == 13) {
+           getTranslation();
+       }
+   });
+});
