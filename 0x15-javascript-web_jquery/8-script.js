7f2cc0187de4ea18f27f34339c42950cca500c2e
Bolexzy <boluoyewumi79@gmail.com>
2023-05-03 19:32:31 +0100

    Add:  fetches and lists the title for all movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function(data) {
+        data.results.forEach(function (film) {
+            $('<LI>').text(film.title).appendTo('UL#list_movies');
+    });
+});
+});
